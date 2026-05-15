e0f1337bdb3d22329b60d3fd54f953cb73641a52
Natan Nardelli <natangabrielnardelli@gmail.com>
2026-05-15 19:22:43 -0300

    Teste

---
+++ EstruturaWhile.java
@@ -0,0 +1,11 @@
+package Whiles;
+public class EstruturaWhile {
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Número: " + i);
+            i++;
+        }
+    }
+}
